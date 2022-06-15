fd7c09324bd3834485405319e340111114084612
Elan Qisthi <elanqisthi@gmail.com>
2022-06-15 22:49:39 +0700

    install and initial setup logging with winston

--- index.js
+++ index.js
@@ -13,12 +13,31 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database(':memory:');
 
+const winston = require('winston');
 const buildSchemas = require('./src/schemas');
+
+const timestampFormat = () => (new Date()).toLocaleDateString();
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new (winston.transports.Console)({
+      timestamp: timestampFormat,
+      colorize: true,
+      level: 'info',
+    }),
+    new (winston.transports.File)({
+      filename: 'error.log',
+      level: 'error',
+    }),
+  ],
+});
 
 db.serialize(() => {
   buildSchemas(db);
 
   const app = require('./src/app')(db);
 
-  app.listen(port, () => console.log(`App started and listening on port ${port}`));
+  app.listen(port, () => logger.info(`App started and listening on port ${port}`));
 });
